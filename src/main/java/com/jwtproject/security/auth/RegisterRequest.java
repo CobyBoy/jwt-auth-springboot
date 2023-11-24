eb6bf1dcb375f48eab4a01baff940a15789627e1
CobyBoy <aboutacoby@gmail.com>
2023-11-24 14:30:48 -0300

    Added constraints to request

--- src/main/java/com/jwtproject/security/auth/RegisterRequest.java
+++ src/main/java/com/jwtproject/security/auth/RegisterRequest.java
@@ -1,13 +1,23 @@
 package com.jwtproject.security.auth;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank
     private String firstName;
+    @NotBlank
     private String lastName;
+    @Email(message = "Invalid email format")
     private String email;
+    @Length(min = 6, max = 20, message = "Password must be between 8 and 50 characters")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
+            message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, and one special character")
     private String password;
 }
