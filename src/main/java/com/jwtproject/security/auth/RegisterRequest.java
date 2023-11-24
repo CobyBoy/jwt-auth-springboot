cde9f86db488f0c07d2d9fdbf68dace89b4b389e
CobyBoy <aboutacoby@gmail.com>
2023-11-24 14:17:20 -0300

    Add register request class

---
+++ src/main/java/com/jwtproject/security/auth/RegisterRequest.java
@@ -0,0 +1,13 @@
+package com.jwtproject.security.auth;
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
