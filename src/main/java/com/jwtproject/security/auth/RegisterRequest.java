e789a2e29daecefc9d674271ea11c1926a74ef3e
CobyBoy <aboutacoby@gmail.com>
2023-11-24 15:17:27 -0300

    Added constraints messages to request and password pattern

--- src/main/java/com/jwtproject/security/auth/RegisterRequest.java
+++ src/main/java/com/jwtproject/security/auth/RegisterRequest.java
@@ -10,13 +10,14 @@ import org.hibernate.validator.constraints.Length;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
-    @NotBlank
+    @NotBlank(message = "You must provide a first name")
     private String firstName;
-    @NotBlank
+    @NotBlank(message = "You must provide a last name")
     private String lastName;
+    @NotBlank(message = "You must provide an email")
     @Email(message = "Invalid email format")
     private String email;
-    @Length(min = 6, max = 20, message = "Password must be between 8 and 50 characters")
+    @Length(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
             message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, and one special character")
     private String password;
