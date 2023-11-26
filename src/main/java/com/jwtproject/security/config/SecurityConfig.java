917d200754877f5abf7464b622c01b625a90a5da
CobyBoy <aboutacoby@gmail.com>
2023-11-25 23:38:23 -0300

    Added security configuration

---
+++ src/main/java/com/jwtproject/security/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.jwtproject.security.config;
+
+import com.jwtproject.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(
+                        (auth) -> {
+                            auth.requestMatchers("/api/v1/auth/**").hasAnyRole();
+                            auth
+                                    .anyRequest()
+                                    .authenticated();
+                            auth.anyRequest().permitAll();
+                        }
+                )
+                .csrf((csrfConfigurer) -> csrfConfigurer.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+
+    }
+}
