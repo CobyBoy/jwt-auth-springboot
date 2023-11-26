426edd54c2d22c7f667520a0718a076bc419d813
CobyBoy <aboutacoby@gmail.com>
2023-11-26 00:02:12 -0300

    Renaming

--- src/main/java/com/jwtproject/security/auth/AuthenticationController.java
+++ src/main/java/com/jwtproject/security/auth/AuthenticationController.java
@@ -18,7 +18,7 @@ public class AuthenticationController {
         return ResponseEntity.ok(authenticationService.register(request));
     }
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
+    public ResponseEntity<AuthenticationResponse> authenticateUser(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
 }
