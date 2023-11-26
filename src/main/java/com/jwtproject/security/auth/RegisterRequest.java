713630ac108ebe48623b31fdbea0701d31e8b2bb
CobyBoy <aboutacoby@gmail.com>
2023-11-26 16:42:59 -0300

    Added h2 database to work on other pc

--- src/main/java/com/jwtproject/security/auth/RegisterRequest.java
+++ src/main/java/com/jwtproject/security/auth/RegisterRequest.java
@@ -17,6 +17,7 @@ public class RegisterRequest {
     @NotBlank(message = "You must provide an email")
     @Email(message = "Invalid email format")
     private String email;
+    @NotBlank(message = "You must provide a password")
     @Length(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
             message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, and one special character")
