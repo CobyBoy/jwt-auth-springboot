df640108c419211f7a7f48baf51c7dd981c3080d
CobyBoy <aboutacoby@gmail.com>
2023-11-27 14:47:12 -0300

    Moved logic to user service

--- src/main/java/com/jwtproject/security/auth/AuthenticationService.java
+++ src/main/java/com/jwtproject/security/auth/AuthenticationService.java
@@ -1,62 +1,26 @@
 package com.jwtproject.security.auth;
 
-import com.jwtproject.security.exception.UserNotFoundException;
 import com.jwtproject.security.jwt.JwtService;
-import com.jwtproject.security.exception.UserAlreadyExistsException;
-import com.jwtproject.security.user.User;
-import com.jwtproject.security.user.UserRepository;
-import com.jwtproject.security.user.UserRoles;
+import com.jwtproject.security.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class AuthenticationService {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
-    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
 
     public AuthenticationResponse signUp(RegisterRequest request) {
-        Optional<User> user = userRepository.findByEmail(request.getEmail());
-        User newUser;
-        if (user.isEmpty()) {
-            newUser = User.builder()
-                    .firstName(request.getFirstName())
-                    .lastName(request.getLastName())
-                    .email(request.getEmail())
-                    .password(passwordEncoder.encode(request.getPassword()))
-                    .userRole(UserRoles.USER)
-                    .build();
-            userRepository.save(newUser);
-            log.info("Saving user: {}", newUser);
-        } else {
-            log.error("User already registered");
-            throw new UserAlreadyExistsException("An account is already registered with your email address. Please log in.");
-        }
-        String jwtToken = jwtService.generateToken(newUser);
         return AuthenticationResponse.builder()
-                .token(jwtToken)
+                .token(jwtService.generateToken(userService.signUp(request)))
                 .build();
     }
 
     public AuthenticationResponse login(AuthenticationRequest request) {
-        log.info("Authenticating user", request.getEmail());
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
-        );
-        User user = userRepository.findByEmail(request.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User was not found"));
-        log.info("Searching user by email: {}", user);
-
-        String jwtToken = jwtService.generateToken(user);
+        String jwtToken = jwtService.generateToken(userService.login(request));
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
