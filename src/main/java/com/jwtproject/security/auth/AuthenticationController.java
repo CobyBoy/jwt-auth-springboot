26b3f8cc74417c03b98b17d82069fc9d8911852d
CobyBoy <aboutacoby@gmail.com>
2023-11-29 03:05:27 -0300

    Added parameter to get uri

--- src/main/java/com/jwtproject/security/auth/AuthenticationController.java
+++ src/main/java/com/jwtproject/security/auth/AuthenticationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 
 @RestController
 @RequestMapping("api/v1/auth")
@@ -17,8 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     @PostMapping("/signup")
-    public ResponseEntity<AuthenticationResponse> signUp(@RequestBody @Valid SignUpRequest request) {
-        return ResponseEntity.ok(authenticationService.signUp(request));
+    public ResponseEntity<AuthenticationResponse> signUp(WebRequest webRequest, @RequestBody @Valid SignUpRequest request) {
+        return ResponseEntity.ok(authenticationService.signUp(request, webRequest));
     }
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody @Valid LoginRequest request) {
