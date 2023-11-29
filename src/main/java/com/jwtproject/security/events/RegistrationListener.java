f22965c25bf8403c24dd0657e009fe4656a9389f
CobyBoy <aboutacoby@gmail.com>
2023-11-29 03:05:53 -0300

    Added registration listener

---
+++ src/main/java/com/jwtproject/security/events/RegistrationListener.java
@@ -0,0 +1,26 @@
+package com.jwtproject.security.events;
+
+import com.jwtproject.security.registration.RegistrationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RegistrationListener implements ApplicationListener<OnRegistrationEvent> {
+    private final RegistrationService registrationService;
+
+    @Override
+    public void onApplicationEvent(OnRegistrationEvent event) {
+        log.info("{}",event.getUserDetails().isEnabled());
+        if (event.getUserDetails().isEnabled()) {
+            log.error("Account enable, email sent and activated");
+        }
+        else {
+            log.info("Account is not enable, send email to confirm registration");
+            registrationService.confirmRegistration(event);
+        }
+    }
+}
