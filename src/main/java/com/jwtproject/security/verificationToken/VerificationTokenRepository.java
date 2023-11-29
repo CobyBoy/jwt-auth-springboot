af1226fc7093d5b98ef554ed41cb5e7f4c300070
CobyBoy <aboutacoby@gmail.com>
2023-11-29 03:05:01 -0300

    Added verification token repo

---
+++ src/main/java/com/jwtproject/security/verificationToken/VerificationTokenRepository.java
@@ -0,0 +1,9 @@
+package com.jwtproject.security.verificationToken;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    VerificationToken findByToken(String token);
+}
