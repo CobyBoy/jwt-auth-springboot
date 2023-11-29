2a577e4223389283e3357a6b69fdf22585d86065
CobyBoy <aboutacoby@gmail.com>
2023-11-29 03:08:39 -0300

    implement event publish

--- src/main/java/com/jwtproject/security/auth/AuthenticationService.java
+++ src/main/java/com/jwtproject/security/auth/AuthenticationService.java
@@ -3,11 +3,15 @@ package com.jwtproject.security.auth;
 import com.jwtproject.security.auth.models.LoginRequest;
 import com.jwtproject.security.auth.models.AuthenticationResponse;
 import com.jwtproject.security.auth.models.SignUpRequest;
+import com.jwtproject.security.events.OnRegistrationEvent;
 import com.jwtproject.security.jwt.JwtService;
 import com.jwtproject.security.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.WebRequest;
 
 @Service
 @RequiredArgsConstructor
@@ -15,11 +19,14 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
     private final JwtService jwtService;
     private final UserService userService;
+    private final ApplicationEventPublisher eventPublisher;
 
-    public AuthenticationResponse signUp(SignUpRequest request) {
+    public AuthenticationResponse signUp(SignUpRequest request, WebRequest webRequest) {
+        UserDetails user = userService.signUp(request);
+        eventPublisher.publishEvent(new OnRegistrationEvent(user, webRequest.getContextPath()));
 
         return AuthenticationResponse.builder()
-                .token(jwtService.generateToken(userService.signUp(request)))
+                .token(jwtService.generateToken(user))
                 .build();
     }
 
