f73a9477d796523f0fe4724e3065ef3e26fb4281
CobyBoy <aboutacoby@gmail.com>
2023-11-29 15:30:03 -0300

    Added auth log entity

---
+++ src/main/java/com/jwtproject/security/audit/AuthenticationLog.java
@@ -0,0 +1,24 @@
+package com.jwtproject.security.audit;
+
+import com.jwtproject.security.user.models.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class AuthenticationLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+    private String ipAddress;
+    private LocalDateTime loggedInAt;
+}
