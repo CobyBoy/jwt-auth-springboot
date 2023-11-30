fe08174e7a9d94eb2f9f67107b87ae4fd175ea68
CobyBoy <aboutacoby@gmail.com>
2023-11-30 10:14:10 -0300

    Added on registration completed event

---
+++ src/main/java/com/jwtproject/security/events/OnRegistrationCompleteEvent.java
@@ -0,0 +1,18 @@
+package com.jwtproject.security.events;
+
+import com.jwtproject.security.user.models.User;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+@Setter
+public class OnRegistrationCompleteEvent extends ApplicationEvent {
+
+    private final User user;
+
+    public OnRegistrationCompleteEvent(User user) {
+        super(user);
+        this.user = user;
+    }
+}
