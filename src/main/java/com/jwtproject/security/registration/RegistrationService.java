fe7636fcd3bf3668f21cfff36f047abdbb4a4b50
CobyBoy <aboutacoby@gmail.com>
2023-11-30 10:23:05 -0300

    Added create registration email method

--- src/main/java/com/jwtproject/security/registration/RegistrationService.java
+++ src/main/java/com/jwtproject/security/registration/RegistrationService.java
@@ -1,27 +1,44 @@
 package com.jwtproject.security.registration;
 
 import com.jwtproject.security.account.AccountActivationService;
+import com.jwtproject.security.email.EmailService;
 import com.jwtproject.security.events.OnRegistrationEvent;
 import com.jwtproject.security.user.models.User;
 import com.jwtproject.security.verificationToken.VerificationToken;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@EnableAsync
 @RequiredArgsConstructor
 public class RegistrationService {
     private final AccountActivationService accountActivationService;
-    @Async
-    public void onSignUpConfirmation(OnRegistrationEvent event) {
-        log.info("confirm registration event {}", event);
+    private final MessageSource messageSource;
+    private final EmailService emailService;
+    @Value("${spring.application.name}")
+    private String appName;
+    public void onUserRegistration(OnRegistrationEvent event) {
+        log.info("confirm sign up event {}", event);
         var user = (User) event.getUserDetails();
         VerificationToken verificationToken = accountActivationService.createVerificationToken(user);
         log.error("User does not have a verification token, create one and send it through email");
-        accountActivationService.sendVerificationEmail(user, verificationToken.getToken(), event.getAppUrl());
+        createRegistrationEmail(user, verificationToken.getToken(), event.getAppUrl());
+    }
+
+    public void createRegistrationEmail(@NotNull User user, String token, String appUrl) {
+        String to = user.getEmail();
+        String confirmationUrl = appUrl + "/api/v1/confirm-account?token=" + token;
+        var subject = messageSource.getMessage("email.confirmation.subject", null, null);
+        var emailBody = messageSource.getMessage("email.confirmation.body", new Object[]{
+                user.getFirstName(),
+                user.getLastName(),
+                confirmationUrl,
+                subject,
+                appName}, null);
+        emailService.sendEmail(to, subject, emailBody);
     }
 }
