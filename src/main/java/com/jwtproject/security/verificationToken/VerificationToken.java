871bc9a1d80bd99ebda09e49b66b948c284ac02d
CobyBoy <aboutacoby@gmail.com>
2023-11-30 09:32:05 -0300

    Added table name

--- src/main/java/com/jwtproject/security/verificationToken/VerificationToken.java
+++ src/main/java/com/jwtproject/security/verificationToken/VerificationToken.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "verification_token")
 public class VerificationToken {
     private static final int EXPIRATION = 60 * 24;
     @Id
